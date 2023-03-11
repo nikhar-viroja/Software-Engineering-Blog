6b41b62c0ae2d1368b329560151f0520a482cabf
Nikhar <nikharashokbhai.viroja@slu.edu>
2023-03-10 20:20:24 -0600

    Week six

---
+++ Python/weeksix/array.py
@@ -0,0 +1,13 @@
+# Create a 1-dimensional array with 5 elements
+my_array = [1, 2, 3, 4, 5]
+
+# Print the array
+print(my_array)
+
+# Access elements in the array
+print(my_array[2])  
+
+# Slicing the array
+print(my_array[:3])  
+print(my_array[3:])  
+print(my_array[1:4])  
